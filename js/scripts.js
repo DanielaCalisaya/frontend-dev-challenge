ba27b6379a511f20b1666246dbe87d3801621c25
DanielaCalisaya <hdaniela127c@gmail.com>
2022-08-02 00:04:44 -0500

    feat(home)added slideshow with testimonies

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,26 @@ const openAndCloseAccordion = document.querySelector(".article__container__faq")
 openAndCloseAccordion.addEventListener("click", function() {
     openAndCloseAccordion.classList.toggle("active")
 })
+
+
+/* Slideshow with testimonies */
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slideshow__container__slide");
+    let dots = document.getElementsByClassName("slideshow__container__dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" act", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " act";
+    setTimeout(showSlides, 5000); // Change text every 5 seconds
+}
